41f6fe0467af58ecf0b58aeeec5de6eac2b97f1d
Sritiman Adak <adaksritiman24@gmail.com>
2024-03-16 22:46:40 +0530

    Add date and status in order confirmation page

--- components/body/order/OrderPageBody.jsx
+++ components/body/order/OrderPageBody.jsx
@@ -1,6 +1,6 @@
-import { Box, Paper, Stack, Typography } from "@mui/material";
-import ReportProblemIcon from '@mui/icons-material/ReportProblem';
-import { grey } from "@mui/material/colors";
+import { Box, Chip, Paper, Stack, Typography } from "@mui/material";
+import ReportProblemIcon from "@mui/icons-material/ReportProblem";
+import { green, grey } from "@mui/material/colors";
 import React from "react";
 import { useContext } from "react";
 import Link from "next/link";
@@ -8,6 +8,7 @@ import OrderShippingAndTotal from "./OrderShippingAndTotal";
 import { CartContext } from "../../../cart/CartProvider";
 import { getFormattedPrice } from "../../common/utils/helpers";
 import CartProductImage from "../cart/CartProductImage";
+import DoneIcon from "@mui/icons-material/Done";
 import { useRouter } from "next/router";
 
 const OrderPageBody = ({ orderData }) => {
@@ -18,25 +19,28 @@ const OrderPageBody = ({ orderData }) => {
 
   if (orderData == undefined || orderData == null) {
     return (
-      <Box sx={{
-        flexGrow:1,
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center"
-      }}>
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Stack
           sx={{
-            display:"flex",
-            flexDirection:"column",
-            alignItems:"center"
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-        <ReportProblemIcon sx={{
-          fontSize:"70px"
-        }}/>
-        <Typography fontWeight={600}>Order Not Found!</Typography>
+          <ReportProblemIcon
+            sx={{
+              fontSize: "70px",
+            }}
+          />
+          <Typography fontWeight={600}>Order Not Found!</Typography>
         </Stack>
-
       </Box>
     );
   }
@@ -69,14 +73,58 @@ const OrderPageBody = ({ orderData }) => {
       >
         {(searchType === undefined || searchType === null) && (
           <>
-            <Typography variant="h4">Your order is confirmed!</Typography>
+            <Typography variant="h4">
+              <DoneIcon sx={{ color: green[800], fontWeight: "bold", pr: 1 }} />
+              Your order is confirmed!
+            </Typography>
             <Typography variant="subtitle1">
               Thank you for shopping with Buzz.
             </Typography>
           </>
         )}
 
-        <Typography variant="h6">Order Number: {orderData.id}</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: {
+              sm: "row",
+              xs: "column",
+            },
+            justifyContent: "space-between",
+          }}
+        >
+          <Box>
+            <Typography variant="h6">
+              Order Number: <strong>{orderData.id}</strong>
+            </Typography>
+            <Box
+              sx={{
+                color: grey[700],
+              }}
+            >
+              <Typography variant="subtitle1">
+                Date: <strong>{orderData.orderDate}</strong>
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "flex-end",
+            }}
+          >
+            <Chip
+              variant="filled"
+              label={`${orderData.status}`}
+              color="success"
+              sx={{
+                background: grey[500],
+                fontSize: "20px",
+              }}
+            />
+          </Box>
+        </Box>
       </Box>
       <Box
         sx={{

0c2778d7d740211ad00eb133fd1341945aca1d94
Sritiman Adak <adaksritiman24@gmail.com>
2024-04-02 23:58:10 +0530

    Add seller information to product in cart

--- components/body/cart/CartPageBody.jsx
+++ components/body/cart/CartPageBody.jsx
@@ -12,8 +12,7 @@ import CartProductImage from "./CartProductImage";
 import AuthContext from "../../../auth/AuthContext";
 import Link from "next/link";
 import { DeliverAddressModelProvider } from "../../../modals/DeliveryAddressModalProvider";
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const CartPageBody = () => {
   const { setNumberOfItems } = useContext(CartContext);
@@ -25,8 +24,12 @@ const CartPageBody = () => {
     removeFromCart,
     addDeliveryAddress,
     placeOrderUsingBankCard,
-  } = useCart(setNumberOfItems, user != null,  user?.username, anonymousAuthSessionId);
-
+  } = useCart(
+    setNumberOfItems,
+    user != null,
+    user?.username,
+    anonymousAuthSessionId
+  );
 
   if (cartData.cartEntryList == null || cartData.cartEntryList.length === 0) {
     return (
@@ -38,7 +41,7 @@ const CartPageBody = () => {
           justifyContent: "center",
           alignItems: "center",
           color: deepPurple[700],
-          flexGrow: 1
+          flexGrow: 1,
         }}
       >
         <Box textAlign="center">
@@ -57,155 +60,176 @@ const CartPageBody = () => {
     );
   }
 
-
   return (
-    <Box sx={{
-      flexGrow: 1
-    }}>
-    <Paper
-      elevation={4}
+    <Box
       sx={{
-        my: "20px",
-        mx: {
-          lg: "250px",
-          md: "20px",
-          xs: "14px",
-        },
-        p: {
-          lg: "50px",
-          md: "22px",
-          xs: "10px",
-        },
-        borderRadius : "4px",
-        background: "white",
+        flexGrow: 1,
       }}
     >
-      <Typography variant="h4" mx="10px"  sx={{ display: "flex", alignItems: "flex-end"}}>
-        <ShoppingCartOutlinedIcon fontSize="32px" sx={{p:"4px"}}/> Your Cart
-      </Typography>
-      {cartData.cartEntryList != null && cartData.cartEntryList.map((product) => (
-        <Paper
+      <Paper
+        elevation={4}
+        sx={{
+          my: "20px",
+          mx: {
+            lg: "200px",
+            md: "50px",
+            xs: "14px",
+          },
+          px: {
+            lg: "130px",
+            md: "75px",
+            xs: "10px",
+          },
+          py: {
+            lg: "50px",
+            md: "22px",
+            xs: "10px",
+          },
+          borderRadius: "4px",
+          background: "white",
+        }}
+      >
+        <Typography
+          variant="h4"
+          mx="10px"
           sx={{
-            m: "10px",
-            mb: "18px",
-            p: "14px",
-            border: `0.5px solid ${grey[400]}`,
-            borderRadius: "4px",
+            p: 2,
             display: "flex",
-            flexDirection: {
-              md: "row",
-              xs: "column",
-            },
-            flexWrap: "no-wrap",
-            background:"white",
+            alignItems: "flex-end",
+            background: deepPurple[50],
+            borderRadius: 2,
+            border: `1px solid ${deepPurple[800]}`,
+            justifyContent: "center"
           }}
-          key={product.id}
         >
-          <CartProductImage images={product.images} />
-          <Box
-            sx={{
-              flexGrow: "1",
-              mt: {
-                md: "0px",
-                xs: "4px",
-              },
-            }}
-          >
-            <Box
+          <ShoppingCartOutlinedIcon fontSize="32px" sx={{ p: "4px" }} /> Your
+          Cart
+        </Typography>
+        {cartData.cartEntryList != null &&
+          cartData.cartEntryList.map((product) => (
+            <Paper
               sx={{
-                "& a" : {
-                  fontFamily: "Trebuchet MS",
-                  fontSize: "20px",
-                  color: grey[800],
-                }
+                m: "10px",
+                mb: "18px",
+                p: "14px",
+                border: `0.5px solid ${grey[400]}`,
+                borderRadius: "4px",
+                display: "flex",
+                flexDirection: {
+                  md: "row",
+                  xs: "column",
+                },
+                flexWrap: "no-wrap",
+                background: "white",
               }}
+              key={product.id}
             >
-              <Link href={`/product/${product.id}`}>
-                {product.name}
-              </Link>
-            </Box>
-           
-            <Typography
-              sx={{
-                fontFamily: "Trebuchet MS",
-                fontWeight: "bold",
-                fontSize: "16px",
-                color: grey[700],
-              }}
-            >
-              {product.brand}
-            </Typography>
-            <CartProductActions
-              product={product}
-              increaseCartQuantityBy1={increaseCartQuantityBy1}
-              decreaseCartQuantityBy1={decreaseCartQuantityBy1}
-              removeFromCart={removeFromCart}
-            />
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              flexDirection : "column",
-              mt: {
-                lg: "4px",
-                xs: "8px",
-              },
-            }}
-          >
-            <Stack
-              sx={{
-                alignSelf : "flex-end"
-              }}
-            >
-              <Typography
+              <CartProductImage images={product.images} />
+              <Box
                 sx={{
-                  fontSize: "15px",
-                  textAlign: {
-                    lg: "right",
-                    xs: "right"   
+                  flexGrow: "1",
+                  mt: {
+                    md: "0px",
+                    xs: "4px",
                   },
-                  color: grey[500],
                 }}
               >
-                Item Price
-              </Typography>
-              <Typography variant="subtitle1">
-                {getFormattedPrice(product.discountedPrice)}
-              </Typography>
-            </Stack>
-            <hr/>
-            <Stack
-              sx={{
-                alignSelf : "flex-end"
-              }}
-            >
-              <Typography
+                <Box
+                  sx={{
+                    "& a": {
+                      fontFamily: "Trebuchet MS",
+                      fontSize: "20px",
+                      color: grey[800],
+                    },
+                  }}
+                >
+                  <Link href={`/product/${product.id}`}>{product.name}</Link>
+                </Box>
+
+                <Typography
+                  sx={{
+                    fontFamily: "Trebuchet MS",
+                    fontWeight: "bold",
+                    fontSize: "16px",
+                    color: grey[700],
+                  }}
+                >
+                  {product.brand}
+                </Typography>
+                <Typography variant="subtitle1" fontSize={"14px"} style={{color : grey[600]}}>
+                  Seller : <strong>{product.seller}</strong>
+                </Typography>
+                <CartProductActions
+                  product={product}
+                  increaseCartQuantityBy1={increaseCartQuantityBy1}
+                  decreaseCartQuantityBy1={decreaseCartQuantityBy1}
+                  removeFromCart={removeFromCart}
+                />
+              </Box>
+              <Box
                 sx={{
-                  textAlign: {
-                    lg: "right",
-                    xs: "right"   
+                  display: "flex",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  mt: {
+                    lg: "4px",
+                    xs: "8px",
                   },
-                  color: grey[500],
                 }}
               >
-                Total Price
-              </Typography>
-              <Typography variant="h6" fontWeight={600}>
-                {getFormattedPrice(product.totalPrice)}
-              </Typography>
-            </Stack>
-          </Box>
-        </Paper>
-      ))}
-      <DeliverAddressModelProvider addDeliveryAddress={addDeliveryAddress}>
-        <ShhippingAndTotal 
-         totalAmount={cartData.totalPrice}
-         deliveryAddress={cartData.deliveryAddress} 
-         placeOrderUsingBankCard={placeOrderUsingBankCard} 
-        />
-      </DeliverAddressModelProvider>
-    </Paper>
+                <Stack
+                  sx={{
+                    alignSelf: "flex-end",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      fontSize: "15px",
+                      textAlign: {
+                        lg: "right",
+                        xs: "right",
+                      },
+                      color: grey[500],
+                    }}
+                  >
+                    Item Price
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    {getFormattedPrice(product.discountedPrice)}
+                  </Typography>
+                </Stack>
+                <hr />
+                <Stack
+                  sx={{
+                    alignSelf: "flex-end",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      textAlign: {
+                        lg: "right",
+                        xs: "right",
+                      },
+                      color: grey[500],
+                    }}
+                  >
+                    Total Price
+                  </Typography>
+                  <Typography variant="h6" fontWeight={600}>
+                    {getFormattedPrice(product.totalPrice)}
+                  </Typography>
+                </Stack>
+              </Box>
+            </Paper>
+          ))}
+        <DeliverAddressModelProvider addDeliveryAddress={addDeliveryAddress}>
+          <ShhippingAndTotal
+            totalAmount={cartData.totalPrice}
+            deliveryAddress={cartData.deliveryAddress}
+            placeOrderUsingBankCard={placeOrderUsingBankCard}
+          />
+        </DeliverAddressModelProvider>
+      </Paper>
     </Box>
   );
 };

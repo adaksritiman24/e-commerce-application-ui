f23761ffe8b13b9faa70e99c023df261eee9b482
Sritiman Adak <adaksritiman24@gmail.com>
2024-10-17 14:25:12 +0530

    Fetch token from localstorage until expired

--- components/constants.js
+++ components/constants.js
@@ -1,5 +1,9 @@
 export const IMAGE_SERVER_BASE_URL= "http://localhost:3003";
 export const SPRING_BOOT_BASE_URL= "http://localhost:8080";
+export const AUTH_TOKEN_DATA= "authTokenData";
+export const EXPIRES_IN= 50;
+export const AUTH_TOKEN= "AUTH_TOKEN";
+export const CREATED_TIME= "CREATED_TIME";
 export const AUTH_TOKEN_URL= "http://localhost:7080/realms/master/protocol/openid-connect/token";
 export const ECOMMERCE_CLIENT_ID= "e-commerce-client-id";
 export const ECOMMERCE_CLIENT_SECRET="hjchKkfcYFojZj1wliCAGeV2qgO6pyps";
--- oauth/client/axiosClient.js
+++ oauth/client/axiosClient.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 import {
+  AUTH_TOKEN,
+  AUTH_TOKEN_DATA,
   AUTH_TOKEN_URL,
+  CREATED_TIME,
   ECOMMERCE_CLIENT_ID,
   ECOMMERCE_CLIENT_SECRET,
+  EXPIRES_IN,
 } from "../../components/constants";
 
 const axiosClient = axios.create({
@@ -12,20 +16,59 @@ const axiosClient = axios.create({
   },
 });
 
-const getOAuth2ClientToken = async () => {
+const setTokenDataInLS = (authToken) => {
+  const tokenData = {
+    AUTH_TOKEN: authToken,
+    CREATED_TIME: Math.floor(Date.now() / 1000),
+  };
+
+  localStorage.setItem(AUTH_TOKEN_DATA, JSON.stringify(tokenData));
+};
+
+const fetchTokenFromServer = async () => {
   console.log("OAuth2 Call invoked");
+  const tokenUrl = AUTH_TOKEN_URL;
+  const data = new URLSearchParams();
+  data.append("grant_type", "client_credentials");
+  data.append("client_id", ECOMMERCE_CLIENT_ID);
+  data.append("client_secret", ECOMMERCE_CLIENT_SECRET);
+  const response = await axios.post(tokenUrl, data.toString(), {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  const authToken = response.data.access_token;
+  if (typeof window !== "undefined") {
+    setTokenDataInLS(authToken);
+  }
+  return authToken;
+};
+
+const getOAuth2ClientToken = async () => {
   try {
-    const tokenUrl = AUTH_TOKEN_URL;
-    const data = new URLSearchParams();
-    data.append("grant_type", "client_credentials");
-    data.append("client_id", ECOMMERCE_CLIENT_ID);
-    data.append("client_secret", ECOMMERCE_CLIENT_SECRET);
-    const response = await axios.post(tokenUrl, data.toString(), {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    });
-    return response.data.access_token;
+    if (typeof window !== "undefined") {
+      console.log("Front-end token call");
+      const authTokenData = localStorage.getItem(AUTH_TOKEN_DATA);
+      if (authTokenData != null || authTokenData != undefined) {
+        const authTokenDataJson = JSON.parse(authTokenData);
+        const currentTime = Math.floor(Date.now() / 1000);
+        const isExpired =
+          currentTime - authTokenDataJson[CREATED_TIME] > EXPIRES_IN;
+        if (isExpired) {
+          console.log("OAuth Token Is Expired");
+          return await fetchTokenFromServer();
+        } else {
+          console.log("OAuth Token Is Valid");
+          return authTokenDataJson[AUTH_TOKEN];
+        }
+      } else {
+        console.log("OAuth Token Not Found");
+        return await fetchTokenFromServer();
+      }
+    } else {
+      console.log("Backend token call");
+      return await fetchTokenFromServer();
+    }
   } catch (error) {
     console.error("Error fetching the OAuth2 token:", error);
     throw error;

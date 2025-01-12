0b81c533ed3ca5ff6208eddcbff8f2ee1ee2eecd
Sritiman Adak <adaksritiman24@gmail.com>
2025-01-12 19:03:36 +0530

    Add delivery address modal at more navigation component

--- components/header/MoreNavigation/MoreNavigation.jsx
+++ components/header/MoreNavigation/MoreNavigation.jsx
@@ -1,259 +1,255 @@
-import { Avatar, Badge, Box, Button, IconButton, Tooltip, Typography, createTheme, useMediaQuery } from '@mui/material'
-import React, { useCallback, useContext } from 'react'
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import InventoryIcon from '@mui/icons-material/Inventory';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { deepOrange, deepPurple, grey } from '@mui/material/colors';
-import AuthContext from '../../../auth/AuthContext';
-import { CartContext } from '../../../cart/CartProvider';
-import { useRouter } from 'next/router';
-import { SignupModalContext } from '../../../modals/payments/SignupModalProvider';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import { getColorCoding } from '../../common/utils/helpers';
-import Image from 'next/image';
+import {
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  IconButton,
+  Tooltip,
+  Typography,
+  createTheme,
+  useMediaQuery,
+} from "@mui/material";
+import React, { useCallback, useContext } from "react";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { deepOrange, deepPurple, grey } from "@mui/material/colors";
+import AuthContext from "../../../auth/AuthContext";
+import { CartContext } from "../../../cart/CartProvider";
+import { useRouter } from "next/router";
+import { SignupModalContext } from "../../../modals/payments/SignupModalProvider";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import { getColorCoding } from "../../common/utils/helpers";
+import Image from "next/image";
+import {
+  DeliverAddressModelProvider
+} from "../../../modals/DeliveryAddressModalProvider";
+import ShipToSection from "./ShipToSection";
 
-const MoreNavigation=({
-    setLoginModalOpen
-})=> {
-    const router = useRouter();
-    const isDesktop  = useMediaQuery('(min-width:1200px)');
-
-    const {user, handleLogout} = useContext(AuthContext);
-    const {numberOfItems} = useContext(CartContext);
-    const {setSignupModalOpen} = useContext(SignupModalContext);
-
-    const badgeTheme = createTheme();
+const MoreNavigation = ({ setLoginModalOpen }) => {
+  const router = useRouter();
+  const isDesktop = useMediaQuery("(min-width:1200px)");
 
 
-    const handleNavigateToCartPage = ()=>{
-        router.push("/cart");
-    }
-    const handleNavigateToOrdersPage = ()=> {
-        router.push('/my-orders');
-    }
-    const getInitials = useCallback((name)=> {
-        var initials = "";
-        var names = name.split(" ");
-        for(let n of names) {
-            initials+=n[0];
-        }
-        return initials;
-    }, [user]);
+  const { user, handleLogout } = useContext(AuthContext);
+  const { numberOfItems } = useContext(CartContext);
+  const { setSignupModalOpen } = useContext(SignupModalContext);
 
-  return (
-    user != null ? (
-        <Box
+  const badgeTheme = createTheme();
+
+  const handleNavigateToCartPage = () => {
+    router.push("/cart");
+  };
+  const handleNavigateToOrdersPage = () => {
+    router.push("/my-orders");
+  };
+  const getInitials = useCallback(
+    (name) => {
+      var initials = "";
+      var names = name.split(" ");
+      for (let n of names) {
+        initials += n[0];
+      }
+      return initials;
+    },
+    [user]
+  );
+
+
+  return user != null ? (
+    <Box
+      sx={{
+        py: "13px",
+        "& svg": {
+          fontSize: isDesktop ? "30px" : "23px",
+        },
+        fontFamily: "arial",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        mx: "10px",
+        width: "100%",
+      }}
+    >
+      <DeliverAddressModelProvider addDeliveryAddress={() => {}}>
+        <ShipToSection user={user} isDesktop={isDesktop} />
+      </DeliverAddressModelProvider>
+
+      <Box
         sx={{
-            py:"13px",
-            "& svg": {
-                fontSize: isDesktop ? "30px" : "23px"
-            },
-            fontFamily:"arial",
+          display: "flex",
+        }}
+      >
+        <Box display="flex" alignItems="center">
+          {null == user.profilePicture ? (
+            <>
+              <Avatar
+                sx={{
+                  bgcolor: getColorCoding(user.name),
+                  width: 32,
+                  height: 32,
+                  fontSize: "12px",
+                  mr: 1,
+                }}
+                variant="rounded"
+              >
+                {getInitials(user.name)}
+              </Avatar>
+            </>
+          ) : (
+            <>
+              <Box
+                sx={{
+                  mr: 1,
+                }}
+              >
+                <Image
+                  width={32}
+                  height={32}
+                  src={user.profilePicture}
+                  style={{
+                    borderRadius: 5,
+                  }}
+                />
+              </Box>
+            </>
+          )}
+          <Typography variant="div">
+            <Typography
+              variant="p"
+              sx={{
+                display: "block",
+                fontSize: "13px",
+              }}
+            >
+              Hello
+            </Typography>
+            <b>{user.name.split(" ")[0]}</b>
+          </Typography>
+        </Box>
+
+        <Tooltip title="Your Orders">
+          <IconButton
+            sx={{
+              ml: 1,
+              color: grey[900],
+            }}
+            onClick={handleNavigateToOrdersPage}
+          >
+            <InventoryIcon />
+          </IconButton>
+        </Tooltip>
+
+        <Tooltip title="Your Cart">
+          <IconButton
+            sx={{
+              ml: 1,
+              color: grey[900],
+            }}
+            onClick={handleNavigateToCartPage}
+          >
+            <Badge badgeContent={numberOfItems} showZero color="error">
+              <ShoppingCartIcon />
+            </Badge>
+          </IconButton>
+        </Tooltip>
+
+        <Tooltip title="Logout">
+          <IconButton
+            sx={{
+              ml: 1,
+              color: grey[900],
+            }}
+            onClick={handleLogout}
+          >
+            <ExitToAppIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+    </Box>
+  ) : (
+    <Box
+      sx={{
+        py: "13px",
+        "& svg": {
+          fontSize: isDesktop ? "30px" : "23px",
+        },
+        fontFamily: "arial",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        mx: "10px",
+        width: "100%",
+      }}
+    >
+      <Box
+        mx={2}
+        sx={{
+          display: "flex",
+        }}
+      >
+        <Box
+          sx={{
             display: "flex",
             alignItems: "center",
-            justifyContent: "space-between",
-            mx : "10px",
-            width : "100%"
-        }}
-    >
-        <Box 
-            mx={2}
-            sx={{
-                display : "flex"
-            }}
+          }}
         >
-            <Box 
-                sx={{
-                    display : "flex",
-                    alignItems : "center"
-                }}
-            >
-                <LocationOnIcon/>
-            </Box>
-            <Box>
-                <Box
-                    sx={{
-                        fontSize : isDesktop ? "17px" : "14px"
-                    }}
-                >Ship To</Box>
-                <Box>
-                    {null != user.address &&
-                    <>
-                        <b style={{ fontSize: isDesktop ? "22px" : "18px"}}>{user.address.city}</b>, 
-                        <span style={{ fontSize : isDesktop ? "18px" : "14px"}}> {user.address.country}</span>
-                    </>
-                    }
-                </Box>
-            </Box>
+          <LocationOnIcon />
         </Box>
+        <Box fontFamily="arial"></Box>
+      </Box>
 
-        <Box
-            sx={{
-                display : "flex"
-            }}
-        > 
-            <Box
-                display="flex"
-                alignItems="center"
-                
-            >
-                {
-                    null == user.profilePicture?
-                    <>
-                        <Avatar sx={{ bgcolor: getColorCoding(user.name) , width: 32, height: 32 , fontSize: "12px" , mr: 1}} variant='rounded'>
-                            {getInitials(user.name)}
-                        </Avatar>
-                    </> :
-                    <>
-                        <Box sx={{
-                            mr: 1
-                        }}>
-                            <Image width={32} height={32} src={user.profilePicture} style={{
-                                borderRadius: 5,
-                            }}/>
-                    </Box>
-                    </>
-                }
-                <Typography variant='div'>
-                    <Typography variant='p'
-                        sx={{
-                            display : "block",
-                            fontSize : "13px",
-                        }}
-                    >
-                        Hello
-                    </Typography>
-                    <b>{user.name.split(" ")[0]}</b>
-                </Typography>
-            </Box>
-
-            <Tooltip title="Your Orders">
-                <IconButton 
-                    sx={{
-                        ml : 1,
-                        color : grey[900]
-                    }}
-                    onClick={handleNavigateToOrdersPage}
-                >
-                    <InventoryIcon/>
-                </IconButton>
-            </Tooltip>
-
-            <Tooltip title="Your Cart">
-                <IconButton
-                    sx={{
-                        ml : 1,
-                        color : grey[900]
-                    }}
-                    onClick={handleNavigateToCartPage}
-                >
-                    <Badge badgeContent={numberOfItems} showZero color="error">
-                    <ShoppingCartIcon/>
-                    </Badge>
-                </IconButton>
-            </Tooltip>
-
-            <Tooltip title="Logout">   
-                <IconButton 
-                    sx={{
-                        ml : 1,
-                        color : grey[900]
-                    }}
-                    onClick={handleLogout}
-                >
-                    <ExitToAppIcon/>
-                </IconButton>
-            </Tooltip>
-        </Box>
-    </Box>
-    ):(
-        <Box
+      <Box
         sx={{
-            py:"13px",
-            "& svg": {
-                fontSize: isDesktop ? "30px" : "23px"
-            },
-            fontFamily:"arial",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            mx : "10px",
-            width : "100%"
+          display: "flex",
         }}
-    >
-        <Box 
-            mx={2}
-            sx={{
-                display : "flex"
-            }}
-        >
-            <Box 
-                sx={{
-                    display : "flex",
-                    alignItems : "center"
-                }}
+      >
+        <Box display="flex" alignItems="center">
+          <Tooltip title="Sign Up">
+            <Button
+              variant="contained"
+              sx={{
+                fontWeight: "bold",
+                textTransform: "none",
+                bgcolor: deepPurple[800],
+                "&: hover": {
+                  bgcolor: deepPurple[900],
+                },
+              }}
+              onClick={() => setSignupModalOpen(true)}
             >
-                <LocationOnIcon/>
-            </Box>
-            <Box fontFamily="arial">
-                
-            </Box>
+              Sign Up
+            </Button>
+          </Tooltip>
         </Box>
 
-        <Box
+        <Tooltip title="Login">
+          <IconButton
             sx={{
-                display : "flex"
+              color: grey[900],
             }}
-        > 
-            <Box
-                display="flex"
-                alignItems="center"
-                
-            >
-                <Tooltip title="Sign Up">
-                    <Button variant='contained' sx={{
-                        fontWeight : "bold",
-                        textTransform : "none",
-                        bgcolor : deepPurple[800],
-                        "&: hover" : {
-                            bgcolor : deepPurple[900]
-                        }
-                    }}
-                    onClick={()=>setSignupModalOpen(true)}
-                    >
-                        Sign Up
-                    </Button>
-                </Tooltip>
-            </Box>
+            onClick={() => setLoginModalOpen(true)}
+          >
+            <AccountBoxIcon />
+          </IconButton>
+        </Tooltip>
 
-            <Tooltip title="Login">
-                <IconButton 
-                    sx={{
-                        color : grey[900]
-                    }}
-                    onClick={()=>setLoginModalOpen(true)}
-                >
-                    <AccountBoxIcon/>
-                </IconButton>
-            </Tooltip>
-
-            <Tooltip title="Your Cart">
-                <IconButton
-                    sx={{
-                        color : grey[900]
-                    }}
-                    onClick={handleNavigateToCartPage}
-                >
-                    <Badge badgeContent={numberOfItems} showZero color="error">
-                        <ShoppingCartIcon/>
-                    </Badge>
-                </IconButton>
-            </Tooltip>
-        </Box>
+        <Tooltip title="Your Cart">
+          <IconButton
+            sx={{
+              color: grey[900],
+            }}
+            onClick={handleNavigateToCartPage}
+          >
+            <Badge badgeContent={numberOfItems} showZero color="error">
+              <ShoppingCartIcon />
+            </Badge>
+          </IconButton>
+        </Tooltip>
+      </Box>
     </Box>
-    )
-  )
-}
+  );
+};
 
 export default MoreNavigation;
---
+++ components/header/MoreNavigation/ShipToSection.jsx
@@ -0,0 +1,71 @@
+import { Box, Tooltip } from "@mui/material";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import React, { useContext } from "react";
+import { DeliveryAddressContext } from "../../../modals/DeliveryAddressModalProvider";
+
+function ShipToSection({user, isDesktop}) {
+    const { setDeliveryAddressOpen } = useContext(DeliveryAddressContext);
+
+    const handleDeliveryAddressChange = () => {
+      setDeliveryAddressOpen(true);
+    };
+    
+
+  return (
+    <Tooltip title="Change delivery address" placement="bottom-start">
+      <Box
+        role="button"
+        sx={{
+          padding: "2px",
+          borderRadius: "5px",
+          cursor: "pointer",
+          "&:hover": {
+            background: "rgba(0,0,0,0.06)",
+          },
+        }}
+        onClick={handleDeliveryAddressChange}
+      >
+        <Box
+          mx={1}
+          sx={{
+            display: "flex",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <LocationOnIcon />
+          </Box>
+          <Box>
+            <Box
+              sx={{
+                fontSize: isDesktop ? "17px" : "14px",
+              }}
+            >
+              Ship To
+            </Box>
+            <Box>
+              {null != user.address && (
+                <>
+                  <b style={{ fontSize: isDesktop ? "22px" : "18px" }}>
+                    {user.address.city}
+                  </b>
+                  ,
+                  <span style={{ fontSize: isDesktop ? "18px" : "14px" }}>
+                    {" "}
+                    {user.address.country}
+                  </span>
+                </>
+              )}
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Tooltip>
+  );
+}
+
+export default ShipToSection;

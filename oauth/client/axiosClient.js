7079cd6abde900b34b0ebe7928497fa2372eaf71
Sritiman Adak <adaksritiman24@gmail.com>
2025-03-22 15:31:45 +0530

    Increae timeout for axios calls

--- oauth/client/axiosClient.js
+++ oauth/client/axiosClient.js
@@ -10,7 +10,7 @@ import {
 } from "../../components/constants";
 
 const axiosClient = axios.create({
-  timeout: 10000,
+  timeout: 30000,
   headers: {
     "Content-Type": "application/json",
   },
